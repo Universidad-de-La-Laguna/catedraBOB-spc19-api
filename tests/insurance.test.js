8824e130cdb605ad84a008efa23d01b7828e990a
Benito Cuesta Viera <benitojcv@iMac-de-Benito.local>
2021-02-09 19:11:52 +0000

    refactor: Simplificacion de los tests

--- tests/insurance.test.js
+++ tests/insurance.test.js
@@ -17,6 +17,24 @@ describe('insurance', function() {
     let server
     let request
 
+    let insuranceData = {
+        id: "d290f1ee-6c54-4b01-90e6-d701748f0851",
+        customers: [
+            {
+                customerId: "customer1",
+                customerName: "My Name",
+                customerTelephone: "555123456",
+                customerEmail: "myemail@example.com"
+            }
+        ],
+        contractDate: "2016-08-29T09:12:33.001Z",
+        startDate: "2016-08-29T09:12:33.001Z",
+        finishDate: "2016-08-29T09:12:33.001Z",
+        assuredPrice: 4.51,
+        negativePcrDate: "2016-08-29T09:12:33.001Z",
+        negativePcrHash: "a3b5543998381d38ee72e2793488d1714c3f8d90f4bda632a411cb32f793bf0a"
+    }
+
     beforeAll(async () => {
         // start db
         await dbHandler.connect()
@@ -30,26 +48,9 @@ describe('insurance', function() {
     describe('POST', function(){
 
         it('Should return json a 201 status and create item', done => {
-            const data = {
-                id: "d290f1ee-6c54-4b01-90e6-d701748f0851",
-                customers: [
-                    {
-                        customerId: "customer1",
-                        customerName: "My Name",
-                        customerTelephone: "555123456",
-                        customerEmail: "myemail@example.com"
-                    }
-                ],
-                contractDate: "2016-08-29T09:12:33.001Z",
-                startDate: "2016-08-29T09:12:33.001Z",
-                finishDate: "2016-08-29T09:12:33.001Z",
-                assuredPrice: 4.51,
-                negativePcrDate: "2016-08-29T09:12:33.001Z",
-                negativePcrHash: "a3b5543998381d38ee72e2793488d1714c3f8d90f4bda632a411cb32f793bf0a"
-            }
 
             request.post('/insurances')
-            .send(data)
+            .send(insuranceData)
             .set('Accept', 'application/json')
             .set('Authorization', 'Bearer ' + adminBearerToken)
             .expect('Content-Type', /json/)
@@ -57,25 +58,12 @@ describe('insurance', function() {
         })
 
         it('Should return 400 error by mandatory data (startDate) is missing', function(done){
-            const data = {
-                id: "d290f1ee-6c54-4b01-90e6-d701748f0851",
-                customers: [
-                    {
-                        customerId: "customer1",
-                        customerName: "My Name",
-                        customerTelephone: "555123456",
-                        customerEmail: "myemail@example.com"
-                    }
-                ],
-                contractDate: "2016-08-29T09:12:33.001Z",
-                finishDate: "2016-08-29T09:12:33.001Z",
-                assuredPrice: 4.51,
-                negativePcrDate: "2016-08-29T09:12:33.001Z",
-                negativePcrHash: "a3b5543998381d38ee72e2793488d1714c3f8d90f4bda632a411cb32f793bf0a"
-            }
+
+            let fakeInsuranceData = Object.assign({}, insuranceData)
+            delete fakeInsuranceData.startDate
 
             request.post('/insurances')
-            .send(data)
+            .send(fakeInsuranceData)
             .set('Accept', 'application/json')
             .set('Authorization', 'Bearer ' + adminBearerToken)
             .expect('Content-Type', /json/)
@@ -83,6 +71,7 @@ describe('insurance', function() {
         })
 
         it('Should return 415 by no body content', function(done){
+
             request.post('/insurances')
             .set('Accept', 'application/json')
             .set('Authorization', 'Bearer ' + adminBearerToken)
@@ -91,26 +80,9 @@ describe('insurance', function() {
         })
 
         it('Should return 409 by insurance already exists', function(done){
-            const data = {
-                id: "d290f1ee-6c54-4b01-90e6-d701748f0851",
-                customers: [
-                    {
-                        customerId: "customer1",
-                        customerName: "My Name",
-                        customerTelephone: "555123456",
-                        customerEmail: "myemail@example.com"
-                    }
-                ],
-                contractDate: "2016-08-29T09:12:33.001Z",
-                startDate: "2016-08-29T09:12:33.001Z",
-                finishDate: "2016-08-29T09:12:33.001Z",
-                assuredPrice: 4.51,
-                negativePcrDate: "2016-08-29T09:12:33.001Z",
-                negativePcrHash: "a3b5543998381d38ee72e2793488d1714c3f8d90f4bda632a411cb32f793bf0a"
-            }
 
             request.post('/insurances')
-            .send(data)
+            .send(insuranceData)
             .set('Accept', 'application/json')
             .set('Authorization', 'Bearer ' + adminBearerToken)
             .expect('Content-Type', /json/)
