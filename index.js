bfa9226f08c79585525e5aca1b6ebeeacc6018b9
Benito Cuesta <bcuestav@ull.edu.es>
2021-04-13 13:22:39 +0100

    fix: Controlar si se ha desplegado o no el contrato general para mostrar mensaje de log al respecto

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const port = parseInt(process.env.PORT, 10) || 8080
 
 appPromise
 .then( (promiseData) => {
-    console.info(`¡¡¡ IMPORTANT !!!: Set environment variable SPC19CONTRACTADDRESS to value ###${promiseData[1]}### the next time you start the service.`)
+    if (typeof promiseData[1] !== "undefined")    
+        console.info(`¡¡¡ IMPORTANT !!!: Set environment variable SPC19CONTRACTADDRESS to value ###${promiseData[1]}### the next time you start the service.`)
 
     // Express listen
     app.listen(port, () => console.log(`Live at ${port}`))
